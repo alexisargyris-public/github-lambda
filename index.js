0926caa6a6401d0123690d64adce6e38f19b21b7
alexisargyris <alexis.argyris@gmail.com>
2016-08-27 14:50:25 +0300

    logic refactoring
    in practice there is no difference

--- index.js
+++ index.js
@@ -21,107 +21,108 @@ exports.handler = (event, context, callback) => {
   // if no command was specified, then exit
   if ((event === undefined) || (event.cmd === undefined) || (event.cmd === '')) {
     callback(new Error('Missing cmd parameter'))
-  }
-  github.authenticate({
-    type: 'oauth',
-    token: token
-  });
-  switch (event.cmd) {
-  case 'getAll':
-    // get all repos of the authenticated user
-    github.repos.getAll({
-      per_page: 100
-    }, (error, response) => {
-      context.callbackWaitsForEmptyEventLoop = false;
-      if (error) { callback(error) } 
-      else { callback(null, response) }
+  } else {
+    github.authenticate({
+      type: 'oauth',
+      token: token
     });
-    break;
-  case 'getCommits':
-    // get the commits of a repo
-    var reponame;
-    // if no repo was provided, then exit
-    if ((event.reponame === undefined)) {
-      callback(new Error('Missing repo/sha parameters'))
-    } else {
-      reponame = event.reponame;
+    switch (event.cmd) {
+    case 'getAll':
+      // get all repos of the authenticated user
+      github.repos.getAll({
+        per_page: 100
+      }, (error, response) => {
+        context.callbackWaitsForEmptyEventLoop = false;
+        if (error) { callback(error) } 
+        else { callback(null, response) }
+      });
+      break;
+    case 'getCommits':
+      // get the commits of a repo
+      var reponame;
+      // if no repo was provided, then exit
+      if ((event.reponame === undefined)) {
+        callback(new Error('Missing repo/sha parameters'))
+      } else {
+        reponame = event.reponame;
+        github.repos.getCommits({
+          user: user,
+          repo: reponame
+        }, (error, response) => {
+          context.callbackWaitsForEmptyEventLoop = false;
+          if (error) { callback(error) } 
+          else { callback(null, response) }
+        });
+      }
+      break;
+    case 'getCommit':
+      // get the content of a commit
+      var reponame;
+      var commitsha;
+      // if no repo/sha were provided, then exit
+      if ((event.reponame === undefined) || (event.commitsha === undefined)) {
+        callback(new Error('Missing repo/sha parameters'))
+      } else {
+        reponame = event.reponame;
+        commitsha = event.commitsha;
+        github.repos.getCommit({
+          user: user,
+          repo: reponame,
+          sha: commitsha
+        }, (error, response) => {
+          context.callbackWaitsForEmptyEventLoop = false;
+          if (error) { callback(error) } 
+          else { callback(null, response) }
+        });
+      }
+      break;
+    case 'getContent':
+      // get the content of a file
+      var reponame;
+      var path;
+      var ref;
+      // if no repo/path/ref were provided, then exit
+      if ((event.reponame === undefined) || (event.path === undefined) || (event.ref === undefined)) {
+        callback(new Error('Missing repo/path/ref parameters'))
+      } else {
+        reponame = event.reponame;
+        path = event.path;
+        ref = event.ref;
+        github.repos.getContent({
+          user: user,
+          repo: reponame,
+          path: path,
+          ref: ref
+        }, (error, response) => {
+          context.callbackWaitsForEmptyEventLoop = false;
+          if (error) { callback(error) } 
+          else { callback(null, response) }
+        });
+      }
+      break;
+    case 'getTree':
+      // get the content of a tree
+      var reponame;
+      var sha;
+      // if no repo/sha were provided, then exit
+      if ((event.reponame === undefined) || (event.sha === undefined)) {
+        callback(new Error('Missing repo/sha parameters'))
+      } else {
+        reponame = event.reponame;
+        sha = event.sha;
+        github.gitdata.getTree({
+          user: user,
+          repo: reponame,
+          sha: sha,
+          recursive: true
+        }, (error, response) => {
+          context.callbackWaitsForEmptyEventLoop = false;
+          if (error) { callback(error) } 
+          else { callback(null, response) }
+        })
+      }
+      break;
+    default:
     }
-    github.repos.getCommits({
-      user: user,
-      repo: reponame
-    }, (error, response) => {
-      context.callbackWaitsForEmptyEventLoop = false;
-      if (error) { callback(error) } 
-      else { callback(null, response) }
-    });
-    break;
-  case 'getCommit':
-    // get the content of a commit
-    var reponame;
-    var commitsha;
-    // if no repo/sha were provided, then exit
-    if ((event.reponame === undefined) || (event.commitsha === undefined)) {
-      callback(new Error('Missing repo/sha parameters'))
-    } else {
-      reponame = event.reponame;
-      commitsha = event.commitsha;
-    }
-    github.repos.getCommit({
-      user: user,
-      repo: reponame,
-      sha: commitsha
-    }, (error, response) => {
-      context.callbackWaitsForEmptyEventLoop = false;
-      if (error) { callback(error) } 
-      else { callback(null, response) }
-    });
-    break;
-  case 'getContent':
-    // get the content of a file
-    var reponame;
-    var path;
-    var ref;
-    // if no repo/path/ref were provided, then exit
-    if ((event.reponame === undefined) || (event.path === undefined) || (event.ref === undefined)) {
-      callback(new Error('Missing repo/path/ref parameters'))
-    } else {
-      reponame = event.reponame;
-      path = event.path;
-      ref = event.ref;
-    }
-    github.repos.getContent({
-      user: user,
-      repo: reponame,
-      path: path,
-      ref: ref
-    }, (error, response) => {
-      context.callbackWaitsForEmptyEventLoop = false;
-      if (error) { callback(error) } 
-      else { callback(null, response) }
-    });
-    break;
-  case 'getTree':
-    // get the content of a tree
-    var reponame;
-    var sha;
-    // if no repo/sha were provided, then exit
-    if ((event.reponame === undefined) || (event.sha === undefined)) {
-      callback(new Error('Missing repo/sha parameters'))
-    } else {
-      reponame = event.reponame;
-      sha = event.sha;
-    }
-    github.gitdata.getTree({
-      user: user,
-      repo: reponame,
-      sha: sha,
-      recursive: true
-    }, (error, response) => {
-      context.callbackWaitsForEmptyEventLoop = false;
-      if (error) { callback(error) } 
-      else { callback(null, response) }
-    })
-    break;
-  default:
   }
 }
