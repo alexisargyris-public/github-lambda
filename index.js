be7b6ed52877b0515a32621647d81708dd2e5796
Alexis Argyris <alexis.argyris@gmail.com>
2016-08-31 10:18:46 +0300

    refactoring to use promisify

--- index.js
+++ index.js
@@ -1,17 +1,18 @@
-var creds = require('./creds.js').creds;
-var GitHubApi = require('github');
-var user;
-var token;
-var github;
+'use strict';
+
+let GitHubApi = require('github');
+let Promise = require('bluebird');
+let creds = require('./creds.js').creds;
 
 exports.handler = (event, context, callback) => {
   // if no command was specified, then exit
   if ((event === undefined) || (event.cmd === undefined) || (event.cmd === '')) {
     callback(new Error('Missing cmd parameter'))
   } else {
-    user = creds.user;
-    token = creds.token;
-    github = new GitHubApi({
+    // init github api
+    let user = creds.user;
+    let token = creds.token;
+    let github = new GitHubApi({
       // required
       version: '3.0.0',
       // optional
@@ -28,95 +29,64 @@ exports.handler = (event, context, callback) => {
       type: 'oauth',
       token: token
     });
+    // main switch
     switch (event.cmd) {
     case 'getAll':
       // get all repos of the authenticated user
-      github.repos.getAll({
-        per_page: 100
-      }, (error, response) => {
-        if (error) { callback(error) } 
-        else { callback(null, response) }
-      });
+      Promise.promisify(github.repos.getAll)({ per_page: 100 })
+        .then((resp) => { debugger; callback(null, response) })
+        .catch((error) => { callback(error) });
       break;
     case 'getCommits':
       // get the commits of a repo
-      var reponame;
       // if no repo was provided, then exit
       if (event.reponame === undefined) {
-        callback(new Error('Missing repo/sha parameters'))
+        callback(new Error('Missing repo parameters'))
       } else {
-        reponame = event.reponame;
-        github.repos.getCommits({
-          user: user,
-          repo: reponame
-        }, (error, response) => {
-          if (error) { callback(error) } 
-          else { callback(null, response) }
-        });
+        let reponame = event.reponame;
+        Promise.promisify(github.repos.getCommits)({ user: user, repo: reponame })
+          .then((resp) => { debugger; callback(null, response) })
+          .catch((error) => { callback(error) });
       }
       break;
     case 'getCommit':
       // get the content of a commit
-      var reponame;
-      var commitsha;
       // if no repo/sha were provided, then exit
       if ((event.reponame === undefined) || (event.commitsha === undefined)) {
         callback(new Error('Missing repo/sha parameters'))
       } else {
-        reponame = event.reponame;
-        commitsha = event.commitsha;
-        github.repos.getCommit({
-          user: user,
-          repo: reponame,
-          sha: commitsha
-        }, (error, response) => {
-          if (error) { callback(error) } 
-          else { callback(null, response) }
-        });
+        let reponame = event.reponame;
+        let commitsha = event.commitsha;
+        Promise.promisify(github.repos.getCommit)({ user: user, repo: reponame, sha: commitsha })
+          .then((resp) => { debugger; callback(null, response) })
+          .catch((error) => { callback(error) });
       }
       break;
     case 'getContent':
       // get the content of a file
-      var reponame;
-      var path;
-      var ref;
       // if no repo/path/ref were provided, then exit
       if ((event.reponame === undefined) || (event.path === undefined) || (event.ref === undefined)) {
         callback(new Error('Missing repo/path/ref parameters'))
       } else {
-        reponame = event.reponame;
-        path = event.path;
-        ref = event.ref;
-        github.repos.getContent({
-          user: user,
-          repo: reponame,
-          path: path,
-          ref: ref
-        }, (error, response) => {
-          if (error) { callback(error) } 
-          else { callback(null, response) }
-        });
+        let reponame = event.reponame;
+        let path = event.path;
+        let ref = event.ref;
+        Promise.promisify(github.repos.getContent)({ user: user, repo: reponame, path: path, ref: ref })
+          .then((resp) => { debugger; callback(null, response) })
+          .catch((error) => { callback(error) });
       }
       break;
     case 'getTree':
       // get the content of a tree
-      var reponame;
-      var sha;
       // if no repo/sha were provided, then exit
       if ((event.reponame === undefined) || (event.sha === undefined)) {
         callback(new Error('Missing repo/sha parameters'))
       } else {
-        reponame = event.reponame;
-        sha = event.sha;
-        github.gitdata.getTree({
-          user: user,
-          repo: reponame,
-          sha: sha,
-          recursive: true
-        }, (error, response) => {
-          if (error) { callback(error) } 
-          else { callback(null, response) }
-        })
+        let reponame = event.reponame;
+        let sha = event.sha;
+        Promise.promisify(github.gitdata.getTree)({ user: user, repo: reponame, sha: sha, recursive: true })
+          .then((resp) => { debugger; callback(null, response) })
+          .catch((error) => { callback(error) });
       }
       break;
     default:
