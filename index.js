6014fb6ecdaceddecd779824eae51e9b6be5577a
Alexis Argyris <alexis.argyris@gmail.com>
2016-12-22 11:58:17 +0200

    github lib update plus a small refactoring

--- index.js
+++ index.js
@@ -1,23 +1,23 @@
 'use strict';
 
-let GitHubApi = require('github');
-let Promise = require('bluebird');
-let creds = require('./creds.js').creds;
-let github;
-let results = [];
-let cb;
-
-function copyAndContinue(error, response) {
-  if (error) {
-    return false;
-  }
-  response.map((item) => { results.push(item); });
-  if (github.hasNextPage(response)) {
-    github.getNextPage(response, { 'user-agent': 'alexisargyris' }, copyAndContinue)
-  } else { cb(null, results); }
-}
-
 exports.handler = (event, context, callback) => {
+  function copyAndContinue(error, response) {
+    if (error) {
+      return false;
+    }
+    response.map((item) => { results.push(item); });
+    if (github.hasNextPage(response)) {
+      github.getNextPage(response, { 'user-agent': 'alexisargyris' }, copyAndContinue)
+    } else { cb(null, results); }
+  }
+
+  let GitHubApi = require('github');
+  let Promise = require('bluebird');
+  let creds = require('./creds.js').creds;
+  let github;
+  let results = [];
+  let cb;
+
   // if no command was specified, then exit
   if ((event === undefined) || (event.cmd === undefined) || (event.cmd === '')) {
     callback(new Error('Missing cmd parameter'))
@@ -59,7 +59,7 @@ exports.handler = (event, context, callback) => {
         callback(new Error('Missing repo parameters'))
       } else {
         let reponame = event.reponame;
-        Promise.promisify(github.repos.getCommits)({ user: user, repo: reponame, per_page: 100 })
+        Promise.promisify(github.repos.getCommits)({ owner: user, repo: reponame, per_page: 100 })
           .then((response) => { copyAndContinue(null, response); })
           .catch((error) => { callback(error); });
       }
@@ -72,7 +72,7 @@ exports.handler = (event, context, callback) => {
       } else {
         let reponame = event.reponame;
         let commitsha = event.commitsha;
-        Promise.promisify(github.repos.getCommit)({ user: user, repo: reponame, sha: commitsha })
+        Promise.promisify(github.repos.getCommit)({ owner: user, repo: reponame, sha: commitsha })
           .then((response) => { callback(null, response) })
           .catch((error) => { callback(error) });
       }
@@ -86,7 +86,7 @@ exports.handler = (event, context, callback) => {
         let reponame = event.reponame;
         let path = event.path;
         let ref = event.ref;
-        Promise.promisify(github.repos.getContent)({ user: user, repo: reponame, path: path, ref: ref })
+        Promise.promisify(github.repos.getContent)({ owner: user, repo: reponame, path: path, ref: ref })
           .then((response) => { callback(null, response) })
           .catch((error) => { callback(error) });
       }
@@ -99,7 +99,7 @@ exports.handler = (event, context, callback) => {
       } else {
         let reponame = event.reponame;
         let sha = event.sha;
-        Promise.promisify(github.gitdata.getTree)({ user: user, repo: reponame, sha: sha, recursive: true })
+        Promise.promisify(github.gitdata.getTree)({ owner: user, repo: reponame, sha: sha, recursive: true })
           .then((response) => { callback(null, response) })
           .catch((error) => { callback(error) });
       }
@@ -109,7 +109,9 @@ exports.handler = (event, context, callback) => {
   }
 }
 
-// exports.handler({
-//   'cmd': 'getCommits',
-//   'reponame': 'amomonaima'
-// });
+exports.handler({
+  // 'cmd': 'getCommits',
+  'cmd': 'getCommit',
+  'reponame': 'amomonaima',
+  'commitsha': '03e171ddb2f1d475b627915d497335ecc9a77c37'
+});
