d19cbfb9da1f8d961bf68be46748ea04e269bf03
Alexis Argyris <alexis.argyris@gmail.com>
2017-03-07 16:40:41 +0200

    implementation of getDoc option
    started using 'markdown-it'

--- index.js
+++ index.js
@@ -1,6 +1,17 @@
 'use strict';
 
+/** 
+ * Simple wrapper to selected github api functions.
+ * @param {*} event -
+ * @param {*} context -
+ * @param {*} callback -
+*/
 exports.handler = (event, context, callback) => {
+  /**
+   * Copy response to results and continue with next page.
+   * @param {*} error - 
+   * @param {*} response - 
+   */
   function copyAndContinue(error, response) {
     if (error) {
       return false;
@@ -10,6 +21,11 @@ exports.handler = (event, context, callback) => {
       github.getNextPage(response, { 'user-agent': 'alexisargyris' }, copyAndContinue)
     } else { cb(null, results); }
   }
+  /**
+   * Checks if target exists in list.
+   * @param {*} target -
+   * @param {*} list -
+   */
   function existsItemInList(target, list) {
     for (var index in list) {
       if (target === list[index].path) return index;
@@ -19,6 +35,7 @@ exports.handler = (event, context, callback) => {
 
   let GitHubApi = require('github');
   let Promise = require('bluebird');
+  let MarkdownIt = require('markdown-it');
   let creds = require('./creds.js').creds;
   let github;
   let results = [];
@@ -28,16 +45,17 @@ exports.handler = (event, context, callback) => {
   let reponame;
   let commitsha;
   let commit;
-  let promises;
   let getTreePrms;
   let getContentPrms;
   let textDecodeError = '<η αποκωδικοποίηση του κείμενου απέτυχε>';
+  let srcRootPath = 'src/';
+  let srcList = [];
 
-  // if no command was specified, then exit
+  // If no command was provided, then exit immediately.
   if ((event === undefined) || (event.cmd === undefined) || (event.cmd === '')) {
     callback(new Error('Missing cmd parameter'))
   } else {
-    // init github api
+    // Init github api.
     user = creds.user;
     token = creds.token;
     github = new GitHubApi({
@@ -57,21 +75,21 @@ exports.handler = (event, context, callback) => {
       type: 'oauth',
       token: token
     });
-    // main switch
+    // Main switch.
     switch (event.cmd) {
-    case 'getAll':
-      // get all repos of the authenticated user
+    case 'getRepos':
+      // Get repos of authenticated user.
       Promise.promisify(github.repos.getAll)({ per_page: 100 })
         .then((response) => { callback(null, response) })
         .catch((error) => { callback(error) });
       break;
     case 'getCommits':
-      // get the commits of a repo
-      // if no repo was provided, then exit
+      // Get commits of repo.
       cb = callback;
       results.length = 0;
+      // If no repo was provided, then exit immediately.
       if (event.reponame === undefined) {
-        callback(new Error('Missing repo parameters'))
+        callback(new Error('Missing repo parameter'))
       } else {
         let reponame = event.reponame;
         Promise.promisify(github.repos.getCommits)({ owner: user, repo: reponame, per_page: 100 })
@@ -80,8 +98,8 @@ exports.handler = (event, context, callback) => {
       }
       break;
     case 'getCommit':
-      // get a commit
-      // if no repo/sha were provided, then exit
+      // Get contents of commit.
+      // If no repo or sha were provided, then exit immediately.
       if ((event.reponame === undefined) || (event.commitsha === undefined)) {
         callback(new Error('Missing repo/sha parameters'))
       } else {
@@ -116,6 +134,39 @@ exports.handler = (event, context, callback) => {
           .catch(error => callback(error));
       }
       break;
+    case 'getDoc':
+      // Get from the latest commit's tree all files whose path starts with 'src' and form a single md doc.
+      // If no repo name was provided, then exit immediately.
+      if (event.reponame === undefined) {
+        callback(new Error('Missing repo parameter'))
+      } else {
+        reponame = event.reponame;
+        Promise.promisify(github.repos.getCommits)({ owner: user, repo: reponame, path: 'src/', page: 1, per_page: 1 })
+          .then((commits) => {
+            commitsha = commits[0].sha;
+            getTreePrms = Promise.promisify(github.gitdata.getTree);
+            getContentPrms = Promise.promisify(github.repos.getContent);
+            return getTreePrms({ owner: user, repo: reponame, sha: commitsha, recursive: true });
+          })
+          .then((tree) => {
+            let promises = [];
+
+            srcList = tree.tree.filter((element, index, array) => { return element.path.startsWith(srcRootPath); })
+            for (var src of srcList) { promises.push( getContentPrms({ owner: user, repo: reponame, path: src.path, ref: commitsha }) ) };
+            return Promise.all(promises);
+          })
+          .then((contents) => {
+            let docContent = '';
+
+            for (var i = 0; i < contents.length; i++) {
+              docContent += '\n\n# ' + srcList[i].path +'\n\n';
+              docContent += new Buffer(contents[i].content, 'base64').toString('utf8');
+            }
+            callback(null, new MarkdownIt().render(docContent));
+          })
+          .catch((error) => { callback(error); });
+      }
+      break;
     default:
     }
   }
@@ -123,9 +174,8 @@ exports.handler = (event, context, callback) => {
 
 /*
   exports.handler({
-    // 'cmd': 'getCommits',
-    'cmd': 'getCommit',
-    'reponame': 'amomonaima',
-    'commitsha': '80b473dec837d3316d0b76960675a5761f9f359a'
+    'cmd': 'getDoc',
+    'reponame': 'amomonaima'
+    // 'commitsha': '80b473dec837d3316d0b76960675a5761f9f359a'
   });
 */
